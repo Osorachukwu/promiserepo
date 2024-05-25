e4fcc04b72cce905ac8611ad1e979030807bfb82
Ezewi Osorachukwu <oezewi@gmail.com>
2024-05-25 11:18:31 +0100

    yyytr

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Buttons from './components/Buttons'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Buttons />
+    </div>
+  )
+}
+
+export default App
